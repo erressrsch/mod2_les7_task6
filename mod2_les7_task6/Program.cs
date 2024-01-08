528f96d141709caf90f6e71b7f97f02e2fbae8fa
Szymon Chyc <erressrsch@gmail.com>
2024-01-08 23:13:45 +0100

    Add project files.

---
+++ mod2_les7_task6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace mod2_les7_task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            if (height >= 140 && height < 165)
+            {
+                Console.WriteLine("You are halfling");
+            }
+
+            if (height >= 165 && height < 195)
+            {
+                Console.WriteLine("You are mugol");
+            }
+            if (height >= 195)
+            {
+                Console.WriteLine("You are Hagrid");
+            }
+        }
+    }
+}
